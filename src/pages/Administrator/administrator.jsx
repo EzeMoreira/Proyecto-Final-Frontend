6e107ba87d8d6dbb892e60ad5e9f0b3be551b189
AugusGarcia <augustoyt21@gmail.com>
2023-10-20 01:37:26 -0300

    fixed page admin

--- src/pages/Administrator/administrator.jsx
+++ src/pages/Administrator/administrator.jsx
@@ -68,7 +68,7 @@ export const Administrator = () => {
 
     setShowForm(false);
     setShowButtons(true);
-    window.location.reload();
+    window.location.href = "/Administrator";
   };
 
   const createMenu = async (menu) => {
